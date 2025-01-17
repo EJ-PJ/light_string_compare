a5e3fe1520cfc2a6dd8c31311f0b8644004a15d9
Josh <jrsosar01@gmail.com>
2025-01-17 11:11:16 -0600

    improving output comparation and adding usage instruccions

--- compare_string.py
+++ compare_string.py
@@ -1,4 +1,6 @@
 import sys
+from colors_text import bcolors
+
 
 def main():
 
@@ -6,22 +8,31 @@ def main():
 
     print("====================COMPARE TWO STRING====================")
 
-    #lower case string 
+    #lower case first string 
     first_string_lw = args_list[1].lower()
 
     #lower case second string 
     second_string_lw = args_list[2].lower()
 
+    print("First string: ", bcolors.WARNING + first_string_lw + bcolors.ENDC)
+
+    print("Second string: ",  bcolors.WARNING + second_string_lw + bcolors.ENDC)
+
     if(first_string_lw == second_string_lw):
-        print("Strings are EQUAL")
+        print(bcolors.OKGREEN + "STRING ARE EQUAL" + bcolors.ENDC)
     else:
-        print("Strings are NOT EQUAL")
+        print(bcolors.FAIL + "STRINGS NOT EQUAL" + bcolors.ENDC)
 
 #returns a list of args
 def get_args():
 
+    #useage program print
+    if((len(sys.argv) == 1)):
+        usage_output()
+        sys.exit(0)
+
     #proofs if there are more than 2 aguments
-    if((len(sys.argv) == 1) or (len(sys.argv) == 2) or (len(sys.argv) > 3)):
+    if((len(sys.argv) == 2) or (len(sys.argv) > 3)):
         print("Error: Too many or very few arguments")
         sys.exit(0)
 
@@ -32,4 +43,12 @@ def get_args():
 
     return arg_list
 
+def usage_output():
+    description = " Light String Compare - compares two simples strings"
+    usage = " USAGE: "
+    args_description = "string_compare string1 string2"
+
+    print(description)
+    print(usage, end="")
+    print(args_description)  
 main()
